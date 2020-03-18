edfc1a0f78695837fee30a4655f0b1cc72000bc2
Roberto88480 <roberto@88480.eu>
2020-03-18 14:13:29 +0100

    initial upload to github

---
+++ bitly.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+#############################################################################
+#                                                                           #
+#       Programm prueft Erreichbarkeit eines Webservers ueber verschiedene  #
+#       Routen und setzt anhand der Ergebnisse die Default-Routen des       #
+#       Systems.                                                            #
+#                                                                           #
+#       Aufruf          : chkroute.sh [ -h | --help ] [ -n | --noop ]       #
+#                                     [ -i | --interactive ]                #
+#                                                                           #
+#       Parameter       : -h | --help: Hilfe                                #
+#                                                                           #
+#                         -n | --noop: Fuehre keine Aenderungen durch,      #
+#                                      zeige nur was getan wuerde           #
+#                                                                           #
+#                         -i | --interactive: Schreibe Log zusaetzlich nach #
+#                                             stdout                        #
+#                                                                           #
+#############################################################################
+set -e
+
+MYPROG=$0
+[ ! -x $MYPROG ] && MYPROG=$(which $0 | awk '{ print $3 }')
+
+
+CURL=$(which curl)
+if [ $? -ne 0 ]
+then
+	>&2 echo "Cannot find curl. Plase make sure it is installed."
+	exit 1;
+fi
+
+JQ=$(which jq)
+if [ $? -ne 0 ]
+then
+	echo "Cannot find jq. Plase make sure it is installed."
+	exit 1;
+fi
+
+APIBASEURL="https://api-ssl.bitly.com/"
+TOKENFILE=~/.config/bitly_api_token
+ACTION=""
+APITOKEN=$(head -n 1 ${TOKENFILE})
+APIRESPONSE=""
+JSONOUTPUT=false
+
+function _usage()
+{
+	grep "^#.*#$" $MYPROG
+	exit $1
+}
+
+function _storeToken()
+{
+	if test -f "$TOKENFILE"; then
+		echo "Stored Token ($FILE) already exists."
+		read -p "Overwrite it? " -n 1 -r
+		echo
+		if [[ ! $REPLY =~ ^[Yy]$ ]]
+		then
+			[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+		fi
+	fi
+	if [[ ${#1} -gt 5 ]]
+	then
+		echo $1 > $TOKENFILE
+	else
+		>&2 echo "Invalid Token"
+		exit 1
+	fi
+}
+
+while [ -n "$1" ]
+do
+	case "$1" in
+		-h|--help)
+			_usage 0
+		;;
+		-t|--use-token)
+			# use specified token instead of stored token
+			shift
+			APITOKEN=$1
+		;;
+		--store-token)
+			# store specified token and exit
+			shift
+			_storeToken $1
+		;;
+		-s|--shorten)
+			shift
+			if [[ $1 == http://* ]] || [[ $1 == https://* ]]
+			then
+				LONGURL=$1
+			else
+				LONGURL='http://'$1
+			fi
+		;;
+		-l|--login)
+			# ask for username and password
+			# query api for access token
+			# save token to userdata
+			echo "Please enter your Bitly.com Username and Password"
+			read -p 'Username: ' username
+			read -sp 'Password: ' password
+			echo
+			if [[ -z "$username" ]] || [[ -z "$password" ]]
+			then
+				echo "Username/Password can not be empty!"
+				exit 1
+			else
+				APIRESPONSE=$($CURL --silent --user "${username}:${password}" --request POST ${APIBASEURL}oauth/access_token)
+				echo "Response: " ${APIRESPONSE}
+			fi
+			break
+		;;
+		-j|--json)
+			# Output response as json
+			JSONOUTPUT=true
+		;;
+		*)
+			_usage 2
+		;;
+	esac
+	shift
+done
+
+if [ -n "$LONGURL" ]
+then
+	if [ ! -n "$APITOKEN" ]
+	then
+		echo "no api token"
+		exit 1
+	fi
+	APIRESPONSE=$($CURL --silent --header "Authorization: Bearer ${APITOKEN}" --header "Content-Type: application/json" --data "{\"long_url\": \"${LONGURL}\"}" https://api-ssl.bitly.com/v4/shorten)
+	if [ "$?" -ne 0 ]
+	then
+		echo "Something went wrong"
+	fi
+	if [ ${JSONOUTPUT} == true ]
+	then
+		echo $APIRESPONSE
+	else
+		echo $APIRESPONSE | jq '.link' | sed -e 's/^"//' -e 's/"$//'
+	fi
+fi
